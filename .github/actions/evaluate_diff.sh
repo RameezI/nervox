3b280942c45666c5afc90dc88920cf10b4d31e80
Rameez Ismail <rameez.ismail@protonmail.com>
2024-10-09 15:23:50 +0200

    suggestions placehoder added

--- .github/actions/evaluate_diff.sh
+++ .github/actions/evaluate_diff.sh
@@ -6,9 +6,9 @@ set -o pipefail
 if git diff --name-only | grep -qE '.*\.py$'; then
     echo "python_changes=true" >> $GITHUB_OUTPUT
     hunks_changed=$(git diff --unified=0 --name-only | grep -E '.*\.py$' | xargs -I {} git diff --unified=0 {} | grep '^@@' | wc -l)
-    files_changed=$(git diff --name-only | grep -E '.*\.py$' | wc -l)
+    files=$(git diff --name-only | grep -E '.*\.py$')
+    echo "files=$files" >> $GITHUB_OUTPUT
     echo "hunks=$hunks_changed" >> $GITHUB_OUTPUT
-    echo "files=$files_changed" >> $GITHUB_OUTPUT
 else
     echo "python_changes=false" >> $GITHUB_OUTPUT
 fi
